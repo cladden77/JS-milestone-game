1ea9735f8d9103093c76d34f02994f83e2889a9f
Chris L <chris.ladden@gmail.com>
2022-05-12 14:09:31 -0500

    add logic for computer to randomly select

--- game.js
+++ game.js
@@ -1,12 +1,37 @@
-const selectionButtons = document.querySelectorAll('[data-selection]')
+const selectionButtons = document.querySelectorAll('[data-selection]');
+
+const selections = [
+    {
+        name: 'rock',
+        emoji: '👊',
+        beats: 'scissors'
+    },
+    {
+        name: 'paper',
+        emoji: '✋',
+        beats: 'rock'
+    },
+    {
+        name: 'scissors',
+        emoji: '✌️',
+        beats: 'paper'
+    },
+]
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
         const selectionName = selectionButton.dataset.selection
-        makeSelection(selectionName)
+        const selection = selections.find(selection => selection.name === selectionName)
+        makeSelection(selection)
     })
 })
 
 function makeSelection(selection) {
-    console.log(selection);
+    const computerSelection = randomSelection()
+    console.log(computerSelection);
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * selections.length)
+    return selections[randomIndex]
 }
